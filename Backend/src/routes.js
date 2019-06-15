a9e23424e3271fd33d126ddcea76c9e79e391420
Uriel <uriel_goncalves@hotmail.com>
2019-06-16 00:08:13 +0100

    Like

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
 const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
 
 const routes = new express.Router();
 const upload = multer(uploadConfig);
@@ -10,6 +11,9 @@ routes.get('/', (req, res)=>{
     return res.send(`Hello ${req.query.name}`);
 });
 
+routes.get('/posts', PostController.index);
+
 routes.post('/posts', upload.single('image'), PostController.store);
+routes.post('/posts/:id/like', LikeController.store);
 
 module.exports = routes;
